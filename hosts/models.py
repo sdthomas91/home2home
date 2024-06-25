fe86726b66eb211cc2bf8fb4052d6af36957da4c
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-25 14:07:30 +0000

    adds Host model, includes a profile pic field

--- hosts/models.py
+++ hosts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Host(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    bio = models.TextField()
+    join_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    def get_profile_picture(self):
+        if self.profile_picture:
+            return self.profile_picture.url
+        else:
+            return '/static/images/default_profile_pic.jpg'
