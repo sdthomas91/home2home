9d57f93ebcc0d9dbd879b24f3c7b95c1be9a26bd
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-25 15:13:19 +0000

    adds additional fields to host model and updates data tables

--- hosts/models.py
+++ hosts/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Host(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/',
+        null=True,
+        blank=True)
     bio = models.TextField()
     join_date = models.DateField()
+    interests = models.CharField(max_length=255, null=True, blank=True)
+    hobbies = models.CharField(max_length=255, null=True, blank=True) 
 
     def __str__(self):
         return self.name
