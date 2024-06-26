288de51b9ffaa1a5dcb0809d651c34d149a841c7
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-26 11:28:23 +0000

    adds updates to user models to allow for host and guest selection - need to update form

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    USER_TYPE_CHOICES = (
+        ('Host', 'Host'),
+        ('Guest', 'Guest'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=5, choices=USER_TYPE_CHOICES)
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
