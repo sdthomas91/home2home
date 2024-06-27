20a888133710add0befe4f85843905824cff8f25
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-27 13:42:41 +0000

    adds model for bookings

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import User
+from properties.models import Property
 
-# Create your models here.
+class Booking(models.Model):
+    guest = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='bookings'
+        )
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        related_name='bookings'
+        )
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Booking: {self.guest.username} - {self.property.title}"
--- properties/models.py
+++ properties/models.py
@@ -3,13 +3,18 @@ from users.models import User
 
 class Amenity(models.Model):
     name = models.CharField(max_length=50)
-    icon = models.CharField(max_length=50, null=True, blank=True)  # For font-awesome icons
+    # include font-awesome icons inspired by Isabella Mitchell Lonely House
+    icon = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class Property(models.Model):
-    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')
+    host = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='properties'
+        )
     title = models.CharField(max_length=200)
     description = models.TextField()
     address = models.CharField(max_length=255)
@@ -17,6 +22,7 @@ class Property(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=20)
+    # Include lat & long for map display
     latitude = models.FloatField()
     longitude = models.FloatField()
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
@@ -28,13 +34,18 @@ class Property(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     featured = models.BooleanField(default=False)
     amenities = models.ManyToManyField(Amenity, blank=True)
-    pets_allowed = models.BooleanField(default=False)  # New field for pets allowed
+    # add pets_allowed to model for guests to set when adding property
+    pets_allowed = models.BooleanField(default=False) 
 
     def __str__(self):
         return self.title
 
 class PropertyImage(models.Model):
-    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='images')
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        related_name='images'
+        )
     image = models.ImageField(upload_to='property_images/')
     description = models.CharField(max_length=255, null=True, blank=True)
 
