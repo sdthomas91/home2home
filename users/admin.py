5acfd91ad5fa174ecef2a1b8c52afcbec13a2994
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-27 13:31:26 +0000

    used django docs to implement filtering in the admin for hosts and gueests to distinguish between the two

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Profile
 
-# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+    list_filter = ('profile__user_type',)  # Add filter for user_type
+
+    def user_type(self, obj):
+        return obj.profile.user_type
+    user_type.admin_order_field = 'profile__user_type'
+    user_type.short_description = 'User Type'
+
+    # Add user_type to the list display
+    def get_list_display(self, request):
+        return self.list_display + ('user_type',)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
