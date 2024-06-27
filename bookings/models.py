9bfd8d5d3739301c7e7f2d8f8a7cf636761688d2
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-27 13:47:09 +0000

    adds some more functionality to booking model t oallow for booking status as well as creation/updating dates. Also adds styling to username for nicer display

--- bookings/models.py
+++ bookings/models.py
@@ -16,6 +16,18 @@ class Booking(models.Model):
     check_in_date = models.DateField()
     check_out_date = models.DateField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    booking_status = models.CharField(
+        max_length=20,
+        choices=[('Pending', 'Pending'),
+        ('Confirmed', 'Confirmed'),
+        ('Cancelled', 'Cancelled')],
+        default='Pending')
+    #  https://www.geeksforgeeks.org/datefield-django-models/
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Booking: {self.guest.username} - {self.property.title}"
+        # capitalize the username for a nicer display
+        return f"Booking: {
+            self.guest.username.capitalize()
+            } - {self.property.title}"
