ce1e550b1e81d1d23bed2d53c45f5a1bb1544841
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-27 14:25:13 +0000

    forgot to save models, adds reviews model including some custom logic for save found in simpleisbetterhtancomplex blog

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import User
+from properties.models import Property
 
-# Create your models here.
+class Review(models.Model):
+    guest = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='reviews')
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='host_reviews')  # New field for host
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review: {self.guest.username} - {self.property.title}"
+
+    def save(self, *args, **kwargs):
+        # automatically set host field based on the property reviewed
+        self.host = self.property.host
+        super().save(*args, **kwargs)
