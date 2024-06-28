d4137844aadbb1097210577bf18e1b0b49e4e6f8
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-28 15:02:50 +0000

    adds basic views to be able to test display of properties

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Property
 
-# Create your views here.
+def all_properties(request):
+    properties = Property.objects.all()
+
+    city = request.GET.get('city')
+    if city:
+        properties = properties.filter(city=city)
+
+    return render(request, 'properties/all_properties.html', {'properties': properties})
