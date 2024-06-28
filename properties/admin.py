9c715b81c206ccd946831efc13dcf2b02e5fda57
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-28 14:46:11 +0000

    finally resolved issue, was with amenity foreign keys. Had accidentally used an invalid key on one of the amenities in the property data. Reuploaded data and now all okay

--- properties/admin.py
+++ properties/admin.py
@@ -1,13 +1,24 @@
+from django import forms
 from django.contrib import admin
+from users.models import User
 from .models import Property, PropertyImage, Amenity
-from .forms import PropertyForm
+
+class PropertyAdminForm(forms.ModelForm):
+    class Meta:
+        model = Property
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['host'].queryset = User.objects.filter(profile__user_type='Host')
+        self.fields['host'].label_from_instance = lambda obj: f"{obj.username} ({obj.profile.bio})"
 
 class PropertyImageInline(admin.StackedInline):
     model = PropertyImage
     extra = 1
 
 class PropertyAdmin(admin.ModelAdmin):
-    form = PropertyForm
+    form = PropertyAdminForm
     inlines = [PropertyImageInline]
     list_display = ('title', 'host', 'city', 'country', 'price_per_night', 'featured')
     list_filter = ('city', 'country', 'featured')
