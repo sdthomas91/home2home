7de954dca4889fba19dfa3c92289b23c97ef389c
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-30 17:02:07 +0000

    adds template and url for adding new property

--- properties/urls.py
+++ properties/urls.py
@@ -4,4 +4,5 @@ from .views import all_properties, property_detail
 urlpatterns = [
     path('properties/', all_properties, name='all_properties'),
     path('properties/<int:property_id>/', property_detail, name='property_detail'),
+    path('properties/add/', add_property, name='add_property'),
 ]
