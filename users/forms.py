87ccf1efcadced5c21e10bd5d80d3818aa2d29af
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-30 05:35:12 +0000

    ammends forms for profile setup and editing

--- users/forms.py
+++ users/forms.py
@@ -4,14 +4,17 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field, Div, Row, Column
 from .models import Profile
 
-# custom signup form to include Guest or Host signup selection
+# Custom signup form to include Guest or Host signup selection
 class CustomSignupForm(SignupForm):
     USER_TYPE_CHOICES = (
         ('Host', 'Host'),
         ('Guest', 'Guest'),
     )
     
-    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES, label="Are you a Host or Guest?")
+    user_type = forms.ChoiceField(
+        choices=USER_TYPE_CHOICES,
+        label="Are you a Host or Guest?"
+        )
 
     def save(self, request):
         user = super().save(request)
@@ -19,30 +22,67 @@ class CustomSignupForm(SignupForm):
         user.profile.save()
         return user
 
-
-    
 # Guest specific profile setup form
-class GuestProfileForm(forms.ModelForm):
-    full_name = forms.CharField()
-    address = forms.CharField(widget=forms.Textarea)
+class GuestProfileSetupForm(forms.ModelForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    address_1 = forms.CharField(label="Address Line 1")
+    address_2 = forms.CharField(label="Address Line 2", required=False)
+    city = forms.CharField()
+    state = forms.CharField()
+    postal_code = forms.CharField(label="Postal Code")
+    country = forms.CharField()
+    bio = forms.CharField(widget=forms.Textarea, required=False)
+    profile_picture = forms.ImageField(required=False)
     default_payment_method = forms.CharField(required=False)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'first_name',
+            'last_name',
+            'address_1',
+            'address_2', 
+            'city',
+            'state',
+            'postal_code',
+            'country',
+            'bio',
+            'profile_picture',
+            'default_payment_method'
+            ]
+
+# Host specific profile setup form
+class HostProfileSetupForm(forms.ModelForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    address_1 = forms.CharField(label="Address Line 1")
+    address_2 = forms.CharField(label="Address Line 2", required=False)
+    city = forms.CharField()
+    state = forms.CharField()
+    postal_code = forms.CharField(label="Postal Code")
+    country = forms.CharField()
+    bio = forms.CharField(widget=forms.Textarea)
+    hobbies = forms.CharField(label="Hobbies (comma separated)")
+    interests = forms.CharField(label="Interests (comma separated)")
     profile_picture = forms.ImageField(required=False)
 
     class Meta:
         model = Profile
-        fields = ['full_name', 'address', 'default_payment_method', 'profile_picture']
-
-#  Host specific profile setup form
-class HostProfileForm(forms.ModelForm):
-    first_name = forms.CharField()
-    address = forms.CharField(widget=forms.Textarea)
-    bio = forms.CharField(widget=forms.Textarea)
-    interests = forms.CharField(widget=forms.Textarea)
-    hobbies = forms.CharField(widget=forms.Textarea)
-
-    class Meta:
-        model = Profile
-        fields = ['first_name', 'address', 'bio', 'interests', 'hobbies']
+        fields = [
+            'first_name',
+            'last_name',
+            'address_1',
+            'address_2',
+            'city',
+            'state',
+            'postal_code',
+            'country',
+            'bio',
+            'hobbies',
+            'interests',
+            'profile_picture'
+            ]
 
 # Profile editing form
 class ProfileEditForm(forms.ModelForm):
