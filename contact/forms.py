52712508fec686e9875852bb304deebd88769d30
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-30 16:46:35 +0000

    adds contact form and ammends styling

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Enter Full Name', 'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Enter Email', 'class': 'form-control'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Type your message...', 'class': 'form-control'}))
+    name = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter Full Name', 'class': 'form-control'}),
+        required=True
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter Email', 'class': 'form-control'}),
+        required=True
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Type your message...', 'class': 'form-control'}),
+        required=True
+    )
--- contact/views.py
+++ contact/views.py
@@ -10,7 +10,7 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             send_mail(
-                form.cleaned_data['subject'],
+                form.cleaned_data['name'],
                 form.cleaned_data['message'],
                 form.cleaned_data['email'],
                 [settings.DEFAULT_FROM_EMAIL],
