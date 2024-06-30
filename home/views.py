a15d2daf4c2889f35de1c748c7db6005db6afcdf
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-30 03:37:29 +0000

    adds the featured property block to home page using similar logic as the all properties page but tweaked slightly

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from properties.models import Property
 
 def home(request):
-    return render(request, 'home/index.html')
+    featured_properties = Property.objects.filter(featured=True)[:4]
+    return render(request, 'home/index.html', {'featured_properties': featured_properties})
