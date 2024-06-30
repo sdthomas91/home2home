aa8f68fc3c303a47aab689ac18ffd6b4b38b425c
Samuel Thomas <samueldthomas91@gmail.com>
2024-06-30 10:29:30 +0000

    adds views and urls for new my_properties page - all functioning well, now need to populate the html

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomSignupView, profile_setup, profile_view, address_edit, profile_edit
+from .views import CustomSignupView, profile_setup, profile_view, address_edit, profile_edit, my_properties
 
 urlpatterns = [
     path('signup/', CustomSignupView.as_view(), name='account_signup'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('profile/address_edit/', address_edit, name='address_edit'),
     path('profile/edit/', profile_edit, name='profile_edit'),
+    path('my_properties/', my_properties, name='my_properties'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CustomSignupForm, ProfileSetupForm, ProfileEditForm, AddressEditForm
 from allauth.account.views import SignupView
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Profile
+from properties.models import Property
 
 class CustomSignupView(SignupView):
+    """
+    A custom view that alters the standard allauth signup process to allow 
+    for custom forms
+    """
     template_name = 'account/signup.html'
     form_class = CustomSignupForm
 
@@ -15,6 +21,10 @@ class CustomSignupView(SignupView):
 
 @login_required
 def profile_setup(request):
+    """
+    A view for the original profile setup to which users are redirected after
+    account creation
+    """
     profile = get_object_or_404(Profile, user=request.user)
     if request.method == 'POST':
         form = ProfileSetupForm(request.POST, request.FILES, instance=profile)
@@ -27,6 +37,9 @@ def profile_setup(request):
 
 @login_required
 def profile_edit(request):
+    """
+    A view allowing users to edit their profile information
+    """
     profile = get_object_or_404(Profile, user=request.user)
     if request.method == 'POST':
         form = ProfileEditForm(request.POST, request.FILES, instance=profile)
@@ -39,12 +52,19 @@ def profile_edit(request):
 
 @login_required
 def profile_view(request):
+    """
+    A view to display the logged in users profile
+    """
     profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'users/profile.html', {'profile': profile})
 
 
 @login_required
 def address_edit(request):
+    """
+    A view to allow users to edit their address details separate to other
+    profile info
+    """
     profile = get_object_or_404(Profile, user=request.user)
     if request.method == 'POST':
         form = AddressEditForm(request.POST, instance=profile)
@@ -54,3 +74,21 @@ def address_edit(request):
     else:
         form = AddressEditForm(instance=profile)
     return render(request, 'users/address_edit.html', {'form': form})
+
+
+@login_required
+def my_properties(request):
+    """
+    A view to render the properties page for the currently logged in host and
+    to redirect logged out or "Guest" users
+    """
+    if request.user.profile.user_type != 'Host':
+        messages.error(request, 'You must be a host to view this page.')
+        return redirect('home')
+    
+    properties = Property.objects.filter(host=request.user)
+    return render(
+        request,
+        'users/my_properties.html',
+        {'properties': properties}
+        )
