dcf5cbc0871f4ae8a6bb6629f53f79f10e79a0f3
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-01 15:33:16 +0000

    adds the review model to admin view

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('guest', 'property', 'rating', 'created_at')
+    search_fields = ('guest__username', 'property__title', 'rating')
+    list_filter = ('rating', 'created_at')
