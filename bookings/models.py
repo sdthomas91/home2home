9648f7bdf74d203464d2441345dae1b91c46a0ec
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-01 03:09:09 +0000

    tidies up booking model and adds basket model

--- bookings/models.py
+++ bookings/models.py
@@ -1,33 +1,44 @@
 from django.db import models
-from users.models import User
+from django.contrib.auth.models import User
 from properties.models import Property
 
 class Booking(models.Model):
-    guest = models.ForeignKey(
+    """
+    Booking model
+    """
+    user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='bookings'
         )
-    property = models.ForeignKey(
-        Property,
-        on_delete=models.CASCADE,
-        related_name='bookings'
-        )
-    check_in_date = models.DateField()
-    check_out_date = models.DateField()
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    guests = models.IntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    booking_status = models.CharField(
-        max_length=20,
-        choices=[('Pending', 'Pending'),
-        ('Confirmed', 'Confirmed'),
-        ('Cancelled', 'Cancelled')],
-        default='Pending')
-    #  https://www.geeksforgeeks.org/datefield-django-models/
+    payment_status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        # capitalize the username for a nicer display
-        return f"Booking: {
-            self.guest.username.capitalize()
-            } - {self.property.title}"
+        return f'Booking {self.id} - {
+            self.property.title
+            } by {self.user.username}'
+
+
+from django.db import models
+from django.contrib.auth.models import User
+from properties.models import Property
+
+class Basket(models.Model):
+    """
+    Basket model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='basket')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    guests = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Basket Item {self.id} - {self.property.title} by {self.user.username}'
