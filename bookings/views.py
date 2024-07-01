fa60d898a45cbdae69cff49f3d3fa45d1509a7af
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-01 03:12:06 +0000

    adds views and forms for booking process - will test once I have setup checkout

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Booking, Property
+from .forms import BookingForm
 
-# Create your views here.
+@login_required
+def book_property(request, property_id):
+    property = Property.objects.get(id=property_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.property = property
+            booking.total_price = property.price_per_night * (booking.check_out - booking.check_in).days
+            booking.save()
+            return redirect('checkout', booking.id)
+    else:
+        form = BookingForm()
+    return render(request, 'book_property.html', {'form': form, 'property': property})
