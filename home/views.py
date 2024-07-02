3e27d1c298d21c7356a4b7d9a24e24bdff38ab2b
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-02 07:39:48 +0000

    adds reviews to home page, just latest reviews

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from properties.models import Property
+from reviews.models import Review
 
 def home(request):
     featured_properties = Property.objects.filter(featured=True)[:4]
-    return render(request, 'home/index.html', {'featured_properties': featured_properties})
+    latest_reviews = Review.objects.all()
+    return render(
+        request,
+        'home/index.html',
+        {
+            'featured_properties': featured_properties,
+            'latest_reviews': latest_reviews
+        }
+        )
