0e59651f1cd929d39cbfe4d63eef01bf8ed87c7c
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-03 09:12:42 +0000

    adds stripe funcionality with card element

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,28 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+  base: {
+      color: '#008080',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+          color: '#6F7378',
+      }
+  },
+  invalid: {
+      color: '#dc3545',
+      iconColor: '#dc3545'
+  }
+};
+var card = elements.create('card', {style: style});
+// var card = elements.create('card');
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -109,7 +109,7 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
