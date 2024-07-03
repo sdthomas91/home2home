8ce8e070d9ad41e63ad76ec33b320b64fa74f921
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-03 07:47:27 +0000

    Adds status to bookings to allow me to add to my bookings in future

--- bookings/models.py
+++ bookings/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 from properties.models import Property
 from django.contrib.auth.models import User
-# https://www.geeksforgeeks.org/python-datetime-timedelta-function/
-from datetime import timedelta
+from datetime import datetime
 
 class Booking(models.Model):
+    # Add status so can add to my bookings
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Confirmed', 'Confirmed'),
+        ('Cancelled', 'Cancelled')
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     checkin = models.DateField()
     checkout = models.DateField()
     guests = models.IntegerField()
     total_nights = models.IntegerField(editable=False, default=0)
-    total_price = models.DecimalField(
-        max_digits=10, decimal_places=2, editable=False, 
-        default=0.0)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, editable=False, default=0.0)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
 
     def save(self, *args, **kwargs):
         # Calculate the number of nights
@@ -24,12 +29,6 @@ class Booking(models.Model):
             
         self.total_nights = (self.checkout - self.checkin).days
         self.total_price = self.total_nights * self.property.price_per_night
-
-        # Print statements for debugging
-        print(f"Check-in date: {self.checkin}")
-        print(f"Check-out date: {self.checkout}")
-        print(f"Total nights: {self.total_nights}")
-        print(f"Total price: {self.total_price}")
 
         super().save(*args, **kwargs)
 
