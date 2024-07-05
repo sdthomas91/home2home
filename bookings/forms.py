85a1aa06c7b8069c54f8b300d57e5fbce1c581af
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-05 04:00:11 +0000

    adds script for booking block on property detail page to prevent past dates being selected

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 
 class BookingForm(forms.Form):
-    checkin = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
-    checkout = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
-    guests = forms.ChoiceField(choices=[(i, i) for i in range(1, 7)], widget=forms.Select(attrs={'class': 'form-control'}))
+    checkin = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'class': 'form-control', 'type': 'date', 'id': 'checkin'}
+            )
+    )
+    checkout = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'class': 'form-control', 'type': 'date', 'id': 'checkout'}
+            )
+    )
+    guests = forms.ChoiceField(
+        choices=[(i, i) for i in range(1, 7)],
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
