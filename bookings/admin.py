ac9b8a1b56c5b4fc3adb9dc3cc0b9b9e23f14e3e
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-05 03:52:11 +0000

    registers bookings app in the admin view

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'property', 'checkin', 'checkout', 'guests', 'total_price', 'status')
+    list_filter = ('status', 'checkin', 'checkout')
+    search_fields = ('user__username', 'property__title')
+    ordering = ('-checkin',)
