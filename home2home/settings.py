15d521d7f97a39d7e7f17f3f9a31218bd6c087f5
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-06 11:07:40 +0000

    adds cache control

--- home2home/settings.py
+++ home2home/settings.py
@@ -199,6 +199,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # AWS
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     AWS_STORAGE_BUCKET_NAME = 'home-2-home'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
