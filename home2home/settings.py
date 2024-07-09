a136ef57d052304ba0cc22201a76d20ca55a52ae
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-09 04:30:33 +0000

    styling and structural changes to site with debug on for live view

--- home2home/settings.py
+++ home2home/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+# DEBUG = True
+
 ALLOWED_HOSTS = ['home-2-home-534807be7c72.herokuapp.com', 'localhost', '.gitpod.io']
 
 
