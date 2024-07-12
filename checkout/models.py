b7e46993fe5f129caf397a36dbc891c715b99f3b
Samuel Thomas <samueldthomas91@gmail.com>
2024-07-12 07:36:28 +0000

    tweaks checkout issues including checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -97,4 +97,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'{self.property.name} on order {self.order.order_number}'
+        return f'{self.property.title} on order {self.order.order_number}'
