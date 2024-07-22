3afc349a6788ca18743d6a228e1c97d90851b3f8
sdthomas91 <130663468+sdthomas91@users.noreply.github.com>
2024-07-22 16:28:26 +0800

    Update webhooks.py
    Update webhooks.py for updated API interface

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -16,8 +16,8 @@ def webhook(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     # Get the webhook data and verify its signature
-    payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    payload = request.body.decode('utf-8')
+    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
     event = None
 
     try:
