2b66c66b2c4207ed796cfd68012a0f0e6dd3be91
sdthomas91 <130663468+sdthomas91@users.noreply.github.com>
2024-07-22 16:18:17 +0800

    Update webhooks.py
    Updated import statement for webhook handler

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
-from .webhook_handler import StripeWH_Handler
+from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
