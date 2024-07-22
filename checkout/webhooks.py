a3bbc00f24b83d0d9ecf41f70c817d32d23df07f
sdthomas91 <130663468+sdthomas91@users.noreply.github.com>
2024-07-22 16:51:33 +0800

    Update webhooks.py
    Updates webhooks for updated API

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -2,9 +2,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-
-from checkout.webhook_handler import StripeWH_Handler
-
+from .webhook_handler import StripeWH_Handler
 import stripe
 
 @require_POST
@@ -16,20 +14,18 @@ def webhook(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     # Get the webhook data and verify its signature
-    payload = request.body.decode('utf-8')
-    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
-        )
+        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
     except ValueError as e:
         # Invalid payload
-        return HttpResponse(status=400)
+        return HttpResponse(content=str(e), status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
-        return HttpResponse(status=400)
+        return HttpResponse(content=str(e), status=400)
     except Exception as e:
         return HttpResponse(content=str(e), status=400)
 
